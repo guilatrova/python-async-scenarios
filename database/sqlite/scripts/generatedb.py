de30489617faa33352a7f9c81d478da447659e28
Guilherme Latrova <hello@guilatrova.dev>
2021-05-31 15:07:05 -0300

    fix: sqlite setup

--- database/sqlite/scripts/generatedb.py
+++ database/sqlite/scripts/generatedb.py
@@ -8,15 +8,10 @@ INSERT_SCRIPT = "./brazil-insert-cities.sql"
 REPEAT_INSERT = 400
 
 def create_db(cursor):
-    db_already_created = os.path.isfile(DB)
-    db_already_created = False
+    with open(BASE_CREATE_SCRIPT, "r") as sql:
+        cursor.executescript(sql.read())
 
-    if not db_already_created:
-        print("Creating database!!!")
-        with open(BASE_CREATE_SCRIPT, "r") as sql:
-            cursor.executescript(sql.read())
-    else:
-        print("Skipping database creation")
+    print("Database created!")
 
 
 def count_cities(cursor):
@@ -24,9 +19,12 @@ def count_cities(cursor):
 
 
 def main():
+    db_already_created = os.path.isfile(DB)
+
     with closing(sqlite3.connect(DB)) as db:
         with closing(db.cursor()) as cursor:
-            create_db(cursor)
+            if not db_already_created:
+                create_db(cursor)
 
             with open(INSERT_SCRIPT, "r") as sql:
                 insert_script = sql.read()
